137ed25ddb07d4f64402a1cbc4250b4924898dc8
gxr404 <1650542259@qq.com>
2025-02-07 18:40:01 +0800

    chore: fix text

--- pages/cryptology/ecc.client.vue
+++ pages/cryptology/ecc.client.vue
@@ -5,14 +5,14 @@ import { Link } from '@element-plus/icons-vue'
 <template>
   <div>
     <div class="pt-6 px-6">
-      <el-link
-        :icon="Link"
-        type="primary"
-        href="https://bitcoin.heapup.tech/cryptology/ecc/ecc"
+      <a
+        class="underline text-[#409eff] hover:text-[#347ecc]"
         target="_blank"
+        href="https://bitcoin.heapup.tech/cryptology/ecc/ecc"
       >
-        <span class="font-bold">&nbsp;建议看 bitcion ninja 的ecc</span>
-      </el-link>
+        <Link class="w-[16px] h-[16px] inline-block align-middle" />
+        <span class="font-bold">&nbsp;建议看 bitcoin ninja 的ecc</span>
+      </a>
     </div>
     <div class="flex flex-wrap gap-6 p-6">
       <cryptology-ecc-base />
